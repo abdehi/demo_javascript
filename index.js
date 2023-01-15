46523b2080f4e73c52ff836a8125f19f33c670d5
Abdehi Perdana Ain <abdehiperdana@gmail.com>
2023-01-15 08:17:53 +0800

    Add: "pengkondisian with function"

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const listMenu = [
   "Gado-gado",
 ];
 
-function show() {
-  console.info(listMenu);
+if (confirm("are you sure to see list menu from us?")) {
+  alert("OK, please look");
+  function show() {
+    document.write(`<h1>Daftar Menu: </h1>`);
+
+    for (let index = 0; index < listMenu.length; index++) {
+      document.write(`<li>${listMenu[index]}</li>`);
+    }
+
+    document.write("</ol>");
+  }
+} else {
+  alert("ok, no problem, maybe next time you could see our menu");
+  output.remove();
+  toggle.remove();
 }
